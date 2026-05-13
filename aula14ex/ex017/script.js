db48d442de3e897541e0119ffac1b8cb155f6532
Joaquim Andrade <166303550+Tharos4768@users.noreply.github.com>
2026-05-13 15:55:30 -0300

    exercicio tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,26 +1,15 @@
+var numI = window.document.getElementById('num');
+var botao = window.document.getElementById('botao');
+var res = window.document.getElementById('res');
 
-window.addEventListener('load', carregar);
+botao.addEventListener('click', tabuada);
 
-function carregar(){
-    var msg = window.document.querySelector('div#msg p');
-    var img = window.document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
-    msg.textContent = `Agora são ${hora} horas`;
+function tabuada(){
+    let num = Number(numI.value);
 
-    if(hora >= 0 && hora < 12){
-        //Bom dia
-        img.src = 'imagens/fotomanha.jpg'
-        document.body.style.backgroundColor = '#f3dbad'
-    }
-    else if(hora >= 12 && hora < 18){
-        //Boa tarde
-        img.src = 'imagens/fototarde.jpg'
-        document.body.style.backgroundColor = '#e0844a';
-    }
-    else{
-        //Boa noite
-        img.src = 'imagens/fotonoite.jpg'
-        document.body.style.backgroundColor = '#333533';
+    res.value = "";
+
+    for(let i = 0 ;i < 11; i++){
+        res.value += `${num} * ${i} = ${num * i}\n`; 
     }
 }
